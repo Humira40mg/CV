252af7cee82c76fb84dfe51c9012cf21b5f8e815
Humira40mg <nathanmillot@icloud.com>
2025-04-10 20:17:52 +1100

    Ajout de la certification C1 Anglais
    Et modification du script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 //variables
 const miaou = new Audio("assets/1095226430826217502.mp3");
+const img = document.querySelector("img");
+let mouseIn = false;
+let intervalId;
+let angle = 0;
+let toupieEnded = true;
 
 //functions
 function randomColor() {
@@ -12,12 +17,31 @@ function randomColor() {
 }
 
 //events listener
-document.querySelector("img").addEventListener("mouseenter", (event) => {
+img.addEventListener("mouseenter", (event) => {
+    if (!toupieEnded) {return;}
+    toupieEnded = false;
+    clearInterval(intervalId);
+    event.target.style.transition = "none";
     event.target.style.transform = "rotate(360deg)";
-    event.target.style.transition = "transform 1s ease-in-out";
+    event.target.style.transition = "transform 1s ease-in";
+  
+    intervalId = setInterval(() => {
+        angle += 360;
+        event.target.style.transform = `rotate(${angle}deg)`;
+        event.target.style.transition = "transform 1s linear";
+    }, 100);
+})
+
+img.addEventListener("mouseleave", (event) => {
+    event.target.style.transition = "none";
+    clearInterval(intervalId);
+    event.target.style.transition = "transform 1s ease-out";
+    event.target.style.transform = `rotate(${angle+360}deg)`;
     setTimeout(() => {
+        angle = 0;
         event.target.style.transition = "none";
         event.target.style.transform = "rotate(0deg)";
+        toupieEnded = true;
     }, 1000);
 })
 
